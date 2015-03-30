38ecf6bdefca49fab50c91ec642c0cb6228a9d7b
Andrew Chilton <andychilton@gmail.com>
2015-03-30 23:05:35 +1300

    First version that merges and processes a timestamp

--- index.js
+++ index.js
@@ -11,16 +11,18 @@ var crypto = require('crypto')
 
 // npm
 var async = require('async')
+var stattoMerge = require('statto-merge')
+var stattoProcess = require('statto-process')
 
 // --------------------------------------------------------------------------------------------------------------------
 
 var queue = async.queue()
-var FIELD_SEP = '~'
-var FIELD_END = '\xff'
+var FIELD_SEP = '!'
+var FIELD_END = '~'
 
 function StattoBackendLevelDB(db) {
   this.db = db
-  this.queue = async.queue(this.process.bind(this), 1)
+  this.queue = async.queue(this.store.bind(this), 1)
 }
 
 StattoBackendLevelDB.prototype.stats = function stats(stats) {
@@ -34,7 +36,7 @@ StattoBackendLevelDB.prototype.stats = function stats(stats) {
   this.queue.push(stats)
 }
 
-StattoBackendLevelDB.prototype.process = function process(stats, done) {
+StattoBackendLevelDB.prototype.store = function store(stats, done) {
   var self = this
 
   console.log('Processing:', JSON.stringify(stats, null, '  '))
@@ -43,52 +45,121 @@ StattoBackendLevelDB.prototype.process = function process(stats, done) {
   var str = JSON.stringify(stats)
   var hash = crypto.createHash('sha1').update(str).digest('hex')
 
-  // firstly, see if this file has already been started (ie. check the hash)
-  var fileKeyBeg = makeFileKey(stats.ts, hash, 'beg')
-  var fileKeyDat = makeFileKey(stats.ts, hash, 'dat')
-  var fileKeyEnd = makeFileKey(stats.ts, hash, 'end')
-  self.db.get(fileKeyBeg, function(err, val) {
+  // Store this file (check if something already there, but not need for locking since if we
+  // overwrite it, it'll have exactly the same content anyway).
+  var fileKey = makeFileKey(stats.ts, hash)
+  self.db.get(fileKey, function(err, val) {
     if (err) {
       if ( err.type !== 'NotFoundError' ) {
         return done(err)
       }
     }
-
-    if (!err) {
-      console.log('This file has already been processed')
-      done()
-    }
-
-    // process this file
-    console.log('Processing Stats ...')
-
-    var ops = [
-      { type : 'put', key : fileKeyBeg, value : (new Date()).toISOString() },
-      { type : 'put', key : fileKeyDat, value : JSON.stringify(stats) },
-    ]
-    self.db.batch(ops, function(err) {
+    self.db.put(fileKey, stats, function(err) {
       if (err) return done(err)
-      console.log('Set started on this file')
-
-      // process the counters first
-      async.parallel(
-        [
-          self.processCounters.bind(self, stats.ts, stats.counters),
-          self.processTimers.bind(self, stats.ts, stats.timers),
-          self.processGauges.bind(self, stats.ts, stats.gauges),
-        ],
-        function(err) {
-          console.log('Finished Processing Stats')
-
-          // let's record that this has finished
-          self.db.put(fileKeyEnd, (new Date()).toISOString(), function(err) {
-            if (err) return done(err)
-            done()
-          })
-        }
-      )
+      done()
     })
   })
+}
+
+StattoBackendLevelDB.prototype.get = function get(date, callback) {
+  var self = this
+
+  var ts = date.toISOString()
+
+  console.log('Getting all of the stats for ' + ts)
+
+  // start streaming all the files for this timestamp
+  var begin = 'f' + FIELD_SEP + ts + FIELD_SEP
+  var end   = 'f' + FIELD_SEP + ts + FIELD_SEP + FIELD_END
+  console.log('begin=%s', begin)
+  console.log('end=%s', end)
+
+  var stats
+
+  var count = 0
+  self.db
+    .createReadStream({ gt : begin, lt : end })
+    .on('data', function (data) {
+      count++
+      console.log('Got some data, key=' + data.key)
+      console.log('             count=' + count)
+      // for the first set of data
+      if ( count === 1 ) {
+        // we don't need to do anything (just remember it)
+        stats = data.value
+      }
+      else {
+        // all subsequent data should be merged
+        stats = stattoMerge({ merged : count }, stats, data.value)
+      }
+    })
+    .on('error', function (err) {
+      console.log('Oh my!', err)
+      callback(err)
+    })
+    .on('close', function () {
+      console.log('Stream Closed')
+    })
+    .on('end', function () {
+      console.log('Stream Ended')
+      if ( !stats ) {
+        return callback() // no problem, but no stats either
+      }
+      callback(null, stats)
+    })
+  ;
+}
+
+StattoBackendLevelDB.prototype.process = function process(date, callback) {
+  var self = this
+
+  var ts = date.toISOString()
+  console.log('Processing for ts=' + ts)
+
+  self.get(date, function(err, stats) {
+    if (err) return callback(err)
+
+    // got the stats
+    console.log('merged stats:', stats)
+
+    // if there are no stats, don't do anything
+    if ( !stats ) {
+      console.log('No stats for ' + ts)
+      return
+    }
+
+    // process the stats
+    stats = stattoProcess(stats)
+
+    // let's store these processed stats
+    var key = 's' + FIELD_SEP + ts
+    self.db.put(key, stats, function(err) {
+      if (err) return callback(err)
+
+      // here, we could denormalise the stats to leveldb ... but perhaps later
+      callback(null, stats)
+    })
+  })
+}
+
+StattoBackendLevelDB.prototype.denormalise = function denormalise(stats, callback) {
+  // process the counters first
+  async.parallel(
+    [
+      self.processCounters.bind(self, stats.ts, stats.counters),
+      self.processTimers.bind(self, stats.ts, stats.timers),
+      self.processGauges.bind(self, stats.ts, stats.gauges),
+    ],
+    function(err) {
+      console.log('Finished Processing Stats')
+
+      // let's record that this has finished
+      self.db.put(fileKeyEnd, (new Date()).toISOString(), function(err) {
+        if (err) return done(err)
+        done()
+      })
+    }
+  )
 }
 
 StattoBackendLevelDB.prototype.processCounters = function processCounters(ts, counters, done) {
@@ -136,6 +207,7 @@ StattoBackendLevelDB.prototype.processGauges = function processGauges(ts, gauges
   var self = this
 
   console.log('Processing gauges ...')
+  console.log(gauges)
 
   // If we have processed a file from a different machine already, then we don't care if a gauge gets overwritten with
   // this info or not. ie. we don't care which one is processed last in this period, since it really shouldn't affect
@@ -155,8 +227,8 @@ StattoBackendLevelDB.prototype.processGauges = function processGauges(ts, gauges
 // --------------------------------------------------------------------------------------------------------------------
 // utility functions
 
-function makeFileKey(ts, hash, type) {
-  return 'f' + FIELD_SEP + ts + FIELD_SEP + hash + FIELD_SEP + type
+function makeFileKey(ts, hash) {
+  return 'f' + FIELD_SEP + ts + FIELD_SEP + hash
 }
 
 function makeCounterKey(name, ts) {
